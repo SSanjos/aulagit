ca7fa74f302d67c770d83430355fc71ebb446900
SSanjos <sandro.sennaanjos@gmail.com>
2022-08-10 22:39:56 -0300

    adicionado correções no arquivo

---
+++ Sala1.java
@@ -0,0 +1,13 @@
+
+
+public class Sala1 {
+
+public static void main(String[] args) {
+	
+	String name = "Sandro";
+
+System.out.println("Meu nome é" + name);
+
+}
+
+}
--- sala1.java
+++
@@ -1,8 +0,0 @@
-public class Sala01 {
-
-String name = "Sandro";
-
-System.out.println("Meu nome é" + name);
-
-
-}
